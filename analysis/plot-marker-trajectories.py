9fa5f366d200be25188ab50bf4bae184a5ecf370
Leif Johnson <leif@leifjohnson.net>
2014-09-28 17:10:51 -0500

    Plot using ndarray explicitly to avoid indexing bug.

--- analysis/plot-marker-trajectories.py
+++ analysis/plot-marker-trajectories.py
@@ -1,5 +1,6 @@
 import climate
 import lmj.plot
+import numpy as np
 
 import source
 import plots
@@ -10,14 +11,18 @@ import plots
     pattern='plot data from files matching this pattern',
     markers='plot traces of these markers',
 )
-def main(root, pattern='*block00/*circuit00.csv.gz', markers='r-fing-index l-fing-index r-heel r-knee'):
+def main(root, pattern='*/*block00/*circuit00.csv.gz', markers='r-fing-index l-fing-index r-heel r-knee'):
     with plots.space() as ax:
         for t in source.Experiment(root).trials:
             if t.matches(pattern):
                 t.load()
                 for i, marker in enumerate(markers.split()):
                     df = t.marker_trajectory(marker)
-                    ax.plot(df.x, df.z, zs=df.y, color=lmj.plot.COLOR11[i], alpha=0.7)
+                    ax.plot(np.asarray(df.x),
+                            np.asarray(df.z),
+                            zs=np.asarray(df.y),
+                            color=lmj.plot.COLOR11[i],
+                            alpha=0.7)
 
 
 if __name__ == '__main__':
