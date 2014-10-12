72865598fbe8658ade68f67da4e2a244a13713b9
Leif Johnson <leif@leifjohnson.net>
2014-10-11 23:01:08 -0500

    Add command-line flags for spline order and accuracy.

--- analysis/plot-marker-trajectories.py
+++ analysis/plot-marker-trajectories.py
@@ -8,14 +8,19 @@ import plots
 
 @climate.annotate(
     root='load experiment data from this directory',
-    pattern='plot data from files matching this pattern',
-    markers='plot traces of these markers',
+    pattern=('plot data from files matching this pattern', 'option'),
+    markers=('plot traces of these markers', 'option'),
+    spline=('interpolate data with a spline of this order', 'option', None, int),
+    accuracy=('fit spline with this accuracy', 'option', None, float),
 )
-def main(root, pattern='*5/*block00/*circuit00.csv.gz', markers='r-fing-index l-fing-index r-heel r-knee'):
+def main(root,
+         pattern='*/*block00/*circuit00.csv.gz',
+         markers='r-fing-index l-fing-index r-heel r-knee',
+         spline=1,
+         accuracy=1):
     with plots.space() as ax:
         for t in source.Experiment(root).trials_matching(pattern):
-            t.realign()
-            t.interpolate()
+            t.normalize(order=spline, accuracy=accuracy)
             for i, marker in enumerate(markers.split()):
                 df = t.trajectory(marker)
                 ax.plot(np.asarray(df.x),
