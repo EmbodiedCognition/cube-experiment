bb347838ad78d61e6a81a1c14657323ad7eeb82b
Leif Johnson <leif@leifjohnson.net>
2014-10-23 14:41:34 -0500

    Add parameter for frame rate.

--- analysis/smooth-data.py
+++ analysis/smooth-data.py
@@ -6,13 +6,14 @@ import database
 @climate.annotate(
     root='load data files from this directory tree',
     output='save smoothed data files to this directory tree',
+    frame_rate=('reindex frames to this rate', 'option', None, float),
     accuracy=('fit SVT with this accuracy', 'option', None, float),
     threshold=('SVT threshold', 'option', None, float),
     frames=('number of frames for SVT', 'option', None, int),
 )
-def main(root, output, accuracy=0.005, threshold=1000, frames=5):
-    for trial in database.Experiment(root).trials_matching('*'):
-        t.reindex()
+def main(root, output, frame_rate=100., accuracy=0.002, threshold=100, frames=5):
+    for t in database.Experiment(root).trials_matching('*'):
+        t.reindex(frame_rate=frame_rate)
         t.svt(threshold, accuracy, frames)
         t.save(t.root.replace(root, output))
 
