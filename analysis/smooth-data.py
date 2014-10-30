217d1b69d27e2dd6a4ff8f953b9f111e406a8443
Leif Johnson <leif@leifjohnson.net>
2014-10-30 16:36:42 -0500

    Low-pass-filter data after filling in with SVT.

--- analysis/smooth-data.py
+++ analysis/smooth-data.py
@@ -11,12 +11,14 @@ import database
     accuracy=('fit SVT with this accuracy', 'option', None, float),
     threshold=('SVT threshold', 'option', None, float),
     frames=('number of frames for SVT', 'option', None, int),
+    lowpass=('lowpass filter at N Hz after SVT', 'option', None, float),
 )
-def main(root, output, frame_rate=100., visibility=0.1, accuracy=0.002, threshold=100, frames=5):
+def main(root, output, frame_rate=100., visibility=0.1, accuracy=0.001, threshold=100, frames=5, lowpass=10.):
     for t in database.Experiment(root).trials_matching('*'):
         t.replace_dropouts(visibility)
         t.reindex(frame_rate)
         t.svt(threshold, accuracy, frames)
+        t.lowpass(lowpass, only_dropouts=False)
         t.save(t.root.replace(root, output))
 
 
