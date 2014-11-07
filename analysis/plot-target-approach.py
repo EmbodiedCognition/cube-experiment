a053751efd7286ef92d32311033399e96827de27
Leif Johnson <leif@leifjohnson.net>
2014-11-07 12:16:32 -0600

    Work out many kinks in plotting marker variance!

--- analysis/plot-target-approach.py
+++ analysis/plot-target-approach.py
@@ -17,44 +17,44 @@ import plots
 )
 def main(root,
          pattern='*/*/*trial00*.csv.gz',
-         markers='13-r-fing-index 25-l-fing-index 40-r-heel 37-r-knee',
+         markers='r-fing-index l-fing-index r-head-front r-heel r-knee',
          target_num=3,
          approach_sec=2):
     data = {(m, s): [] for m in markers.split() for s in range(12)}
     num_frames = int(100 * approach_sec)
-    frames = list(range(num_frames))
     targets = None
     for trial in database.Experiment(root).trials_matching(pattern):
         if targets is None:
             targets = trial
         move = trial.movement_to(target_num)
-        move.normalize(frame_rate=100, order=1)
+        move.df = pd.DataFrame(np.asarray(move.df),
+                               columns=move.df.columns,
+                               index=np.arange(1 - len(move.df), 1))
         for marker, source in data:
             if all(move.df.source == source):
-                arr = move.df.loc[:, marker + '-x':marker + '-z']
-                data[marker, source].append(pd.DataFrame(
-                    np.asarray(arr)[len(move.df) - num_frames::-1],
-                    columns=list('xyz'),
-                    index=frames[:len(move.df)]))
+                data[marker, source].append(move.trajectory(marker))
     with plots.space() as ax:
-        plots.show_cubes(ax, targets)
+        plots.show_cubes(ax, targets, target_num=target_num)
         for i, (marker, keys) in enumerate(itertools.groupby(sorted(data), lambda x: x[0])):
             for j, (_, source) in enumerate(keys):
                 dfs = data[marker, source]
                 if not dfs:
                     continue
                 for df in dfs:
-                    ax.plot(np.asarray(df.x),
-                            np.asarray(df.z),
-                            zs=np.asarray(df.y),
+                    ax.plot(np.asarray(df[-num_frames:].x),
+                            np.asarray(df[-num_frames:].z),
+                            zs=np.asarray(df[-num_frames:].y),
                             color=lmj.plot.COLOR11[i],
-                            alpha=0.7)
+                            alpha=0.3)
                 merged = pd.concat(dfs, axis=1, keys=list(range(len(dfs)))).groupby(axis=1, level=1)
                 mu = merged.mean()
                 sigma = merged.std()
-                for t in np.linspace(0, num_frames - 1, 3).astype(int):
-                    x, y, z = plots.ellipsoid([mu.x[t], mu.y[t], mu.z[t]],
-                                              [sigma.x[t], sigma.y[t], sigma.z[t]])
+                for t in np.linspace(0, num_frames, 5).astype(int):
+                    try:
+                        x, y, z = plots.ellipsoid([mu.x[-t], mu.y[-t], mu.z[-t]],
+                                                  [sigma.x[-t], sigma.y[-t], sigma.z[-t]])
+                    except:
+                        continue
                     ax.plot_wireframe(x, z, y, color=lmj.plot.COLOR11[i], alpha=0.7)
 
 
