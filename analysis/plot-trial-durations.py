81578eb29ceaf12d6e4ebb6ed96f39e2a32cb8d1
Leif Johnson <leif@leifjohnson.net>
2014-11-09 19:51:45 -0600

    Get trial durations plot in better shape.

--- analysis/plot-trial-durations.py
+++ analysis/plot-trial-durations.py
@@ -1,43 +1,62 @@
 import climate
 import collections
+import hashlib
 import lmj.plot
 import numpy as np
+import os
+import pickle
 
 import database
 import plots
-
 
 @climate.annotate(
     root='load experiment data from this directory',
     pattern=('plot data from files matching this pattern', 'option'),
 )
 def main(root, pattern='*.csv.gz'):
-    data = collections.defaultdict(lambda: collections.defaultdict(list))
-    for s in database.Experiment(root).subjects:
-        for i, b in enumerate(s.blocks):
-            for j, t in enumerate(b.trials):
-                if t.matches(pattern):
-                    t.load()
-                    data[i][j].append(t.df.index[-1] / t.total_distance)
-    counts = []
-    means = []
-    stds = []
+    tmpfile = 'plot-trial-durations-{}.pkl'.format(
+        hashlib.md5((root + pattern).encode('utf8')).hexdigest())
+    if os.path.exists(tmpfile):
+        with open(tmpfile, 'rb') as handle:
+            data = pickle.load(handle)
+    else:
+        data = collections.defaultdict(lambda: collections.defaultdict(list))
+        for s in database.Experiment(root).subjects:
+            for i, b in enumerate(s.blocks):
+                for j, t in enumerate(b.trials):
+                    if t.matches(pattern):
+                        t.load()
+                        data[i][j].append(t.df.index[-1] / t.total_distance)
+        for i in data:
+            data[i] = dict(data[i])
+        with open(tmpfile, 'wb') as handle:
+            pickle.dump(dict(data), handle)
     labels = []
-    for i in sorted(data):
-        for j in sorted(data[i]):
-            counts.append(len(data[i][j]))
-            means.append(np.mean(data[i][j]))
-            stds.append(np.std(data[i][j]))
-            labels.append('{}/{}'.format(i + 1, j + 1))
-    xs = np.arange(len(means))
-    means = np.array(means)
-    stds = np.array(stds)
-    with plots.plot() as ax:
-        ax.plot(xs, means, color='#111111')
-        ax.fill_between(xs, means - stds, means + stds,
-                        color='#111111', alpha=0.7, lw=0)
-        ax.set_xticks(xs)
+    with lmj.plot.with_axes(spines=True) as ax:
+        t = 0
+        for i in sorted(data):
+            means = []
+            stds = []
+            for j in sorted(data[i]):
+                labels.append(str(j + 1))
+                d = np.array(data[i][j])
+                z = (d - d.mean()) / d.std()
+                # discard outliers more than 3 stdevs from mean
+                valid = d[abs(z) < 3]
+                means.append(valid.mean())
+                stds.append(valid.std())
+                ax.scatter(len(labels) - 1 + 0.1 * np.random.randn(len(valid)), valid,
+                           c=lmj.plot.COLOR11[0], facecolors='none', lw=1, alpha=0.7)
+            u = t + len(means)
+            ax.errorbar(range(t, u), means, yerr=stds, color=lmj.plot.COLOR11[0], lw=2)
+            t = u
+        ax.set_xticks(range(len(labels)))
         ax.set_xticklabels(labels)
+        ax.set_xlabel('Trial')
+        ax.set_xlim(-0.5, len(labels) - 0.5)
+        ax.set_ylabel('Normalized Duration (s/m)')
+        ax.set_yticks([1, 2, 3])
+        ax.set_yticklabels(['1', '2', '3'])
 
 
 if __name__ == '__main__':
