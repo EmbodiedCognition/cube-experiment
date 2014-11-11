ffb2189f899d3a781a1bac434ad22bfa4e3ab0e9
Leif Johnson <leif@leifjohnson.net>
2014-11-11 16:31:37 -0600

    Use pickled-data api for plotting target approach.

--- analysis/plot-target-approach.py
+++ analysis/plot-target-approach.py
@@ -8,6 +8,23 @@ import database
 import plots
 
 
+@database.pickled
+def process_data(root, pattern, markers, target_num):
+    data = {(m, s): [] for m in markers.split() for s in range(12)}
+    targets = None
+    for trial in database.Experiment(root).trials_matching(pattern):
+        if targets is None:
+            targets = trial
+        move = trial.movement_to(target_num)
+        move.df = pd.DataFrame(np.asarray(move.df),
+                               columns=move.df.columns,
+                               index=np.arange(1 - len(move.df), 1))
+        for marker, source in data:
+            if all(move.df.source == source):
+                data[marker, source].append(move.trajectory(marker))
+    return data, targets
+
+
 @climate.annotate(
     root='read subject data from this file tree',
     pattern=('plot data from files matching this pattern', 'option'),
@@ -20,19 +37,8 @@ def main(root,
          markers='r-fing-index l-fing-index r-head-front r-heel r-knee',
          target_num=3,
          approach_sec=2):
-    data = {(m, s): [] for m in markers.split() for s in range(12)}
     num_frames = int(100 * approach_sec)
-    targets = None
-    for trial in database.Experiment(root).trials_matching(pattern):
-        if targets is None:
-            targets = trial
-        move = trial.movement_to(target_num)
-        move.df = pd.DataFrame(np.asarray(move.df),
-                               columns=move.df.columns,
-                               index=np.arange(1 - len(move.df), 1))
-        for marker, source in data:
-            if all(move.df.source == source):
-                data[marker, source].append(move.trajectory(marker))
+    data, targets = process_data(root, pattern, markers, target_num)
     with plots.space() as ax:
         plots.show_cubes(ax, targets, target_num=target_num)
         for i, (marker, keys) in enumerate(itertools.groupby(sorted(data), lambda x: x[0])):
