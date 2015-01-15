a375802de94bcf2ad1cf4669aa2c9a99923f631e
Leif Johnson <leif@lmjohns3.com>
2015-01-15 16:24:03 -0600

    Use collections.namedtuple to pass args around.

--- analysis/smooth-data.py
+++ analysis/smooth-data.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 
 import climate
+import collections
 import multiprocessing as mp
 
 import database
 
 
 def smooth(args):
-    trial, root, output, frame_rate, visibility, accuracy, threshold, frames, lowpass = args
-    trial.replace_dropouts(visibility)
-    trial.reindex(frame_rate)
-    trial.svt(threshold, 4 * accuracy, frames)
-    trial.lowpass(1 * lowpass, only_dropouts=True)
-    trial.svt(threshold, 2 * accuracy, frames)
-    trial.lowpass(2 * lowpass, only_dropouts=True)
-    trial.svt(threshold, 1 * accuracy, frames)
-    trial.lowpass(4 * lowpass, only_dropouts=False)
-    trial.save(trial.root.replace(root, output))
+    args.trial.replace_dropouts(args.visibility)
+    args.trial.reindex(args.frame_rate)
+    args.trial.svt(args.threshold, 4 * args.accuracy, args.frames)
+    args.trial.lowpass(1 * args.lowpass, only_dropouts=True)
+    args.trial.svt(args.threshold, 2 * args.accuracy, args.frames)
+    args.trial.lowpass(2 * args.lowpass, only_dropouts=True)
+    args.trial.svt(args.threshold, 1 * args.accuracy, args.frames)
+    args.trial.lowpass(4 * args.lowpass, only_dropouts=False)
+    args.trial.save(args.trial.root.replace(args.root, args.output))
 
+
+Args = collections.namedtuple('Args', 'trial root output frame_rate visibility accuracy threshold frames lowpass')
 
 @climate.annotate(
     root='load data files from this directory tree',
@@ -27,12 +29,12 @@ def smooth(args):
     accuracy=('fit SVT with this accuracy', 'option', None, float),
     threshold=('SVT threshold', 'option', None, float),
     frames=('number of frames for SVT', 'option', None, int),
-    lowpass=('lowpass filter at N Hz after SVT', 'option', None, float),
+    lowpass=('lowpass filter at N Hz', 'option', None, float),
 )
-def main(root, output, frame_rate=100., visibility=0.1, accuracy=0.001, threshold=100, frames=5, lowpass=10.):
+def main(root, output, frame_rate=100., visibility=0.1, accuracy=0.001, threshold=100, frames=3, lowpass=5.):
     args = root, output, frame_rate, visibility, accuracy, threshold, frames, lowpass
     trials = database.Experiment(root).trials_matching('*')
-    mp.Pool().map(smooth, ((t, ) + args for t in trials))
+    mp.Pool().map(smooth, (Args(t, *args) for t in trials))
 
 
 if __name__ == '__main__':
