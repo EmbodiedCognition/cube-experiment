14d6955118893c532c1d9f8f6037d1da1b18dbbb
Leif Johnson <leif@lmjohns3.com>
2015-01-25 19:58:10 -0600

    Add multiple skeletons for the moment.

--- analysis/plot-skeleton.py
+++ analysis/plot-skeleton.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import climate
+import pandas as pd
 
 import database
 import plots
@@ -10,10 +11,15 @@ import plots
     root='plot data rooted at this path',
     pattern=('plot data from files matching this pattern', 'option'),
 )
-def main(root, pattern='*/*block02/*trial00*.csv.gz'):
+def main(root, pattern='*/*block03/*trial00*.csv.gz'):
     for trial in database.Experiment(root).trials_matching(pattern):
         with plots.space() as ax:
-            plots.skeleton(ax, trial, 100)
+            for i in range(3):
+                plots.skeleton(ax, trial, 1000 + 300 * i, lw=2, color='#fd3220', alpha=0.3)
+            #trial.rotate_heading(pd.Series([-6.28 / 10] * len(trial.df)))
+            trial.make_body_relative()
+            for i in range(3):
+                plots.skeleton(ax, trial, 1000 + 300 * i, offset=(0.5 * i, 0.5 * i), lw=2, color='#111111', alpha=0.3)
 
 
 if __name__ == '__main__':
