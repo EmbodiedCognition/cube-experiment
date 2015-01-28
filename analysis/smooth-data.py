234a193ab042c0d0f3e9ddd8a53c7d742eebce51
Leif Johnson <leif@lmjohns3.com>
2015-01-28 13:33:31 -0600

    Use noisy linear interpolation for error in SVT.
    I think this is getting closer to the "right way" to do this.

--- analysis/smooth-data.py
+++ analysis/smooth-data.py
@@ -5,18 +5,48 @@ import collections
 import joblib
 import numpy as np
 import pandas as pd
+import scipy.signal
 
 import database
 
+logging = climate.get_logger('smooth')
+
 
 def marker_channel_columns(df, min_filled=0):
+    '''Return the names of columns that contain marker data.'''
     return [c for c in df.columns
             if c.startswith('marker') and
             c[-1] in 'xyz' and
             df[c].count() > min_filled * len(df)]
 
 
-def svt(df, threshold=500, max_rmse=0.002, consec_frames=3, log_every=0):
+def closest_observation(series):
+    '''Compute the distance (in frames) to the nearest non-dropout frame.
+
+    Parameters
+    ----------
+    series : pd.Series
+        A Series holding a single channel of mocap data.
+
+    Returns
+    -------
+    closest : pd.Series
+        An integer series containing, for each frame, the number of frames
+        to the nearest non-dropout frame.
+    '''
+    drops = series.isnull()
+    closest_l = [0]
+    for d in drops:
+        closest_l.append(1 + closest_l[-1] if d else 0)
+    closest_r = [0]
+    for d in drops[::-1]:
+        closest_r.append(1 + closest_r[-1] if d else 0)
+    return pd.Series(
+        list(map(min, closest_l[1:], reversed(closest_r[1:]))),
+        index=series.index)
+
+
+def svt(df, threshold=500, max_rmse=0.002, learning_rate=1, log_every=0):
     '''Complete missing marker data using singular value thresholding.
 
     This method alters the given `df` in-place.
@@ -36,61 +66,41 @@ def svt(df, threshold=500, max_rmse=0.002, consec_frames=3, log_every=0):
     max_rmse : float, optional
         Continue the reconstruction process until reconstructed data is
         below this RMS error compared with measured data. Defaults to 0.002.
-    consec_frames : int, optional
-        Compute the SVT using trajectories of this many consecutive frames.
-        Defaults to 3.
+    learning_rate : float, optional
+        Make adjustments to objective function with step size. Defaults to 1.
     log_every : int, optional
         Number of SVT iterations between logging output. Defaults to 0,
         which only logs output at the start and finish of the SVT process.
     '''
     cols = marker_channel_columns(df, min_filled=0.01)
+    data = df[cols]
+    num_entries = data.shape[0] * data.shape[1]
+    logging.info('SVT: missing %d of (%d, %d) = %d values (%.1f%% filled)',
+                 num_entries - data.count().sum(),
+                 data.shape[0], data.shape[1], num_entries,
+                 100 * data.count().sum() / num_entries)
 
-    marker_data = df[cols]
-    num_frames, num_markers = marker_data.shape
-    num_entries = num_frames * num_markers
-    filled_ratio = marker_data.count().sum() / max(1e-3, num_entries)
+    msg = 'SVT %d: rmse %f using %d singular values'
+    log = lambda: logging.info(msg, i, rmse, len(s.nonzero()[0]))
 
-    vals = marker_data.values
-    extra = num_frames % consec_frames
-    if extra > 0:
-        fill = np.zeros((consec_frames - extra, num_markers))
-        fill.fill(float('nan'))
-        vals = np.vstack([vals, fill])
-    df = pd.DataFrame(vals.reshape((len(vals) // consec_frames, num_markers * consec_frames)))
-
-    logging.info('SVT: filling %d x %d, reshaped as %d x %d',
-                 num_frames, num_markers, df.shape[0], df.shape[1])
-    logging.info('SVT: missing %d of %d values (%.1f%% filled)',
-                 num_entries - marker_data.count().sum(),
-                 num_entries,
-                 100 * filled_ratio)
-
-    def log():
-        logging.info('SVT %d: weighted rmse %f using %d singular values',
-                     i, rmse, len(s.nonzero()[0]))
-
-    def noise():
-        return (max_rmse / 2) * np.random.randn(*err.shape)
-
+    stdevs = max_rmse * closest_observation(data.iloc[:, 0])[:, None]
+    linear = data.interpolate().ffill().bfill().values
     s = None
-    x = y = pd.DataFrame(np.zeros_like(df))
+    x = y = np.zeros_like(linear)
     rmse = max_rmse + 1
     i = 0
     while rmse > max_rmse:
-        err = (df - x).fillna(0)
-        y += (1.2 / filled_ratio) * (err + noise())
+        err = linear - x + stdevs * np.random.randn(*x.shape)
+        y += learning_rate * err
         u, s, v = np.linalg.svd(y, full_matrices=False)
         s = np.clip(s - threshold, 0, np.inf)
-        x = pd.DataFrame(np.dot(u, np.dot(np.diag(s), v)))
+        x = np.dot(u, np.dot(np.diag(s), v))
         rmse = np.sqrt((err * err).mean().mean())
         if log_every and i % log_every == 0: log()
         i += 1
     log()
 
-    x = np.asarray(x).reshape((-1, num_markers))[:num_frames]
-    filled = pd.DataFrame(x, index=marker_data.index, columns=marker_data.columns)
-    for c in cols:
-        df[c] = filled[c]
+    df[cols] = x
 
 
 def lowpass(df, freq=10., order=4):
