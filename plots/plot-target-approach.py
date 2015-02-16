6857cdb3ff30ef239040680123e1d8a7d3251641
Leif Johnson <leif@lmjohns3.com>
2015-02-15 23:36:05 -0600

    Update target-approach plot.
    Still want to turn this data into a 2D plot of variance (sum of axis
    stderrs?) over time, too!

--- plots/plot-target-approach.py
+++ plots/plot-target-approach.py
@@ -2,68 +2,78 @@
 
 import climate
 import itertools
+import lmj.cubes
 import lmj.plot
 import numpy as np
 import pandas as pd
 
-import database
-import plots
-
-
-@database.pickled
-def target_approaches(root, pattern, markers, target_num):
-    data = {(m, s): [] for m in markers.split() for s in range(12)}
-    targets = None
-    for trial in database.Experiment(root).trials_matching(pattern):
-        if targets is None:
-            targets = trial
-        move = trial.movement_to(target_num)
-        move.df = pd.DataFrame(np.asarray(move.df),
-                               columns=move.df.columns,
-                               index=np.arange(1 - len(move.df), 1))
-        for marker, source in data:
-            if all(move.df.source == source):
-                data[marker, source].append(move.trajectory(marker))
-    return data, targets
-
+MARKERS = 'r-fing-index l-fing-index r-heel   r-head-front'
+_COLORS = '#111111      #d62728      #1f77b4  #2ca02c'
+COLORS = dict(zip(MARKERS.split(), _COLORS.split()))
 
 @climate.annotate(
     root='read subject data from this file tree',
     pattern=('plot data from files matching this pattern', 'option'),
-    markers=('plot data for these mocap markers', 'option'),
+    output=('save movie in this output filename', 'option'),
     target=('plot data for this target', 'option', None, int),
     approach_sec=('plot variance for N sec prior to target acquisition', 'option', None, float),
 )
-def main(root,
-         pattern='*/*/*trial00*.csv.gz',
-         markers='r-fing-index l-fing-index r-head-front r-heel r-knee',
-         target=3,
-         approach_sec=2):
-    num_frames = int(100 * approach_sec)
-    data, targets = process_data(root, pattern, markers, target)
-    with plots.space() as ax:
-        plots.show_cubes(ax, targets, target_num=target_num)
-        for i, (marker, keys) in enumerate(itertools.groupby(sorted(data), lambda x: x[0])):
-            for j, (_, source) in enumerate(keys):
-                dfs = data[marker, source]
-                if not dfs:
-                    continue
-                for df in dfs:
-                    ax.plot(np.asarray(df[-num_frames:].x),
-                            np.asarray(df[-num_frames:].z),
-                            zs=np.asarray(df[-num_frames:].y),
-                            color=lmj.plot.COLOR11[i],
-                            alpha=0.3)
-                merged = pd.concat(dfs, axis=1, keys=list(range(len(dfs)))).groupby(axis=1, level=1)
-                mu = merged.mean()
-                sigma = merged.std()
-                for t in np.linspace(0, num_frames, 5).astype(int):
-                    try:
-                        x, y, z = plots.ellipsoid([mu.x[-t], mu.y[-t], mu.z[-t]],
-                                                  [sigma.x[-t], sigma.y[-t], sigma.z[-t]])
-                    except:
-                        continue
-                    ax.plot_wireframe(x, z, y, color=lmj.plot.COLOR11[i], alpha=0.7)
+def main(root, pattern='*/*/*trial00*', output=None, target=3, approach_sec=1):
+    targets = None
+
+    # first, read trial data and extract marker trajectories. group by marker
+    # and source cube.
+    data = {}
+    for trial in lmj.cubes.Experiment(root).trials_matching(pattern):
+        trial.load()
+        targets = trial
+        move = trial.movement_to(target)
+        move.df.index = pd.Index(np.arange(1 - len(move.df), 1))
+        source = move.df.source.iloc[0]
+        for marker in MARKERS.split():
+            data.setdefault((marker, source), []).append(move.trajectory(marker))
+
+    # next, compute mean and stderr for every time step approaching targets.
+    # group again by marker and source cube.
+    agg = {}
+    for key, dfs in data.items():
+        keys = list(range(len(dfs)))
+        merged = pd.concat(dfs, axis=1, keys=keys).groupby(axis=1, level=1)
+        agg[key] = merged.mean(), merged.std() / np.sqrt(merged.size())
+
+    def show(ax):
+        ax.w_xaxis.set_pane_color((1, 1, 1, 1))
+        ax.w_yaxis.set_pane_color((1, 1, 1, 1))
+        ax.w_zaxis.set_pane_color((1, 1, 1, 1))
+        ax.set_xlim(-2, 2)
+        ax.set_xticks([-2, -1, 0, 1, 2])
+        ax.set_xticklabels([])
+        ax.set_ylim(-2, 2)
+        ax.set_yticks([-2, -1, 0, 1, 2])
+        ax.set_yticklabels([])
+        ax.set_zlim(0, 2)
+        ax.set_zticks([0, 1, 2])
+        ax.set_zticklabels([])
+        num_frames = int(100 * approach_sec)
+        def render():
+            lmj.cubes.plots.show_cubes(ax, targets, target_num=target)
+            for (marker, source), (mean, stderr) in agg.items():
+                mx, my, mz = mean.x, mean.y, mean.z
+                sx, sy, sz = stderr.x, stderr.y, stderr.z
+                for t in np.linspace(0, num_frames, 7).astype(int):
+                    x, y, z = lmj.cubes.plots.ellipsoid(
+                        [mx[-t], my[-t], mz[-t]],
+                        [sx[-t], sy[-t], sz[-t]])
+                    ax.plot_wireframe(x, z, y, color=COLORS[marker], alpha=0.3, lw=1)
+        return render
+
+    anim = lmj.plot.rotate_3d(
+        show, output=output,
+        azim=(0, 90), elev=(5, 20),
+        fig=dict(figsize=(10, 4.8)),
+    )
+    if not output:
+        lmj.plot.show()
 
 
 if __name__ == '__main__':
