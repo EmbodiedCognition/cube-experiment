6553840edf7e2009d332102c75f31c693cf724c5
Leif Johnson <leif@lmjohns3.com>
2015-02-18 01:17:41 -0600

    Replace SVD-based filling (too slow!) with factorization that runs on GPU.

--- analysis/01-fill-dropouts.py
+++ analysis/01-fill-dropouts.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 import climate
-import joblib
 import lmj.cubes
 import numpy as np
 import pandas as pd
-import pypropack
 import scipy.signal
+import theano
+import theano.tensor as TT
 
 logging = climate.get_logger('fill')
 
@@ -59,45 +59,38 @@ MARKERS = [
 ]
 
 
-def marker_channel_columns(df, min_filled=0):
+def marker_channel_columns(df):
     '''Return the names of columns that contain marker data.'''
     return [c for c in df.columns if c[:-2] in MARKERS and c[-1] in 'xyz']
 
 
-def svt(df, threshold=None, tol=1e-3, learning_rate=1.5, window=10):
-    '''Complete missing marker data using singular value thresholding.
+def fill(df, tol=1e-4, window=20, gamma=1):
+    '''Complete missing marker data using matrix completion.
 
     This method alters the given `df` in-place.
-
-    Singular value thresholding is described in Cai, Candes, & Shen (2010),
-    "A Singular Value Thresholding Algorithm for Matrix Completion" (see
-    http://arxiv.org/pdf/0810.3286.pdf). The implementation here is rather
-    naive but seems to get the job done for the types of mocap data that we
-    gathered in the cube experiment.
 
     Parameters
     ----------
     df : pd.DataFrame
         Data frame for source data.
-    threshold : float, optional
-        Threshold for singular values. Defaults to the "natural" falling-off
-        point in the singular value spectrum.
     tol : float, optional
-        Continue the reconstruction process until reconstructed data is
-        within this relative tolerance threshold. Defaults to 1e-3.
-    learning_rate : float, optional
-        Make adjustments to objective function with step size. Defaults to 1.5.
+        Continue the reconstruction process until reconstructed data is within
+        this relative tolerance threshold. Defaults to 1e-4.
     window : int, optional
-        Model windows of this many consecutive frames. Defaults to 10.
+        Model windows of this many consecutive frames. Defaults to 20.
+    gamma : float, optional
+        Use this parameter to regularize the low-rank entries on the factored
+        data matrix. Defaults to 1.
     '''
     cols = marker_channel_columns(df)
     data = df[cols]
     num_frames, num_channels = data.shape
     num_entries = num_frames * num_channels
-    logging.info('SVT: missing %d of (%d, %d) = %d values (%.1f%% filled)',
+    filled_ratio = data.count().sum() / num_entries
+    logging.info('missing %d of (%d, %d) = %d values (%.1f%% filled)',
                  num_entries - data.count().sum(),
                  num_frames, num_channels, num_entries,
-                 100 * data.count().sum() / num_entries)
+                 100 * filled_ratio)
 
     filled = data.fillna(0).values
     weights = (~data.isnull()).values.astype(float)
@@ -121,42 +114,57 @@ def svt(df, threshold=None, tol=1e-3, learning_rate=1.5, window=10):
     # this is more or less like a convolution with a sliding rectangular window.
     # this stacked data matrix is the one we'll want to fill in the SVT process
     # below.
-    w = np.asfortranarray(np.concatenate([
-        weights[i:num_frames-(window-1-i)] for i in range(window)], axis=1).astype('f'))
-    t = np.asfortranarray(np.concatenate([
-        filled[i:num_frames-(window-1-i)] for i in range(window)], axis=1).astype('f'))
-    norm_t = np.linalg.norm(t)
+    w = np.concatenate([weights[i:num_frames-(window-1-i)] for i in range(window)], axis=1).astype(np.int8)
+    t = np.concatenate([filled[i:num_frames-(window-1-i)] for i in range(window)], axis=1).astype('f')
+    logging.info('processing windowed data %s', t.shape)
 
-    logging.info('SVT: processing windowed data %s', t.shape)
+    rank = 600
+    s = scipy.linalg.svd(t, compute_uv=False)
+    cdf = (s / s.sum()).cumsum()
+    logging.info('reconstructing with rank %d (%d%%) %s',
+                 rank, 100 * cdf[rank],
+                 [(f, cdf.searchsorted(f)) for f in (0.5, 0.8, 0.9, 0.99)])
 
-    # if the threshold is none, set it using the falloff point in the spectrum.
-    if threshold is None:
-        s = pd.Series(pypropack.svdp(
-            t, k=20, kmax=200, compute_u=False, compute_v=False)[0])
-        threshold = s[s.diff().diff().shift(-1).argmax()]
-        logging.info('SVT: using threshold %.2f', threshold)
+    # optimization parameters
+    gamma = TT.cast(gamma, 'float32')
+    learning_rate = TT.cast(0.0001, 'float32')
+    momentum = TT.cast(0.94, 'float32')
+    max_norm = TT.cast(100000, 'float32')
+    one = TT.cast(1, 'float32')
 
-    x = None
-    y = 0 + t
-    i = topk = 0
-    inck = 5
-    while True:
+    # symbolic variables.
+    data = TT.matrix('data')
+    mask = TT.bmatrix('mask')
+
+    # model parameters.
+    u = theano.shared(np.random.randn(t.shape[0], rank).astype('f'), name='u')
+    v = theano.shared(np.random.randn(rank, t.shape[1]).astype('f'), name='v')
+
+    # symbolic computations, including sgd with momentum.
+    err = data - TT.dot(u, v)
+    sqerr = (mask * err * err).sum()
+    sqdat = (mask * data * data).sum()
+    loss = sqerr + gamma * ((u * u).sum() + (v * v).sum())
+    updates = []
+    for param, grad in zip([u, v], TT.grad(loss, [u, v])):
+        g = grad * TT.minimum(one, max_norm / TT.sqrt((grad * grad).sum()))
+        v = theano.shared(np.zeros_like(param.get_value()), name='v' + param.name)
+        vnew = momentum * v - learning_rate * g
+        updates.append((v, vnew))
+        updates.append((param, param + vnew))
+    f = theano.function([data, mask], [sqerr, sqerr / sqdat], updates=updates)
+
+    i = 0
+    n = w.sum()
+    rerr = tol + 1
+    while rerr > tol:
+        aerr, rerr = f(t, w)
+        if not i % 10:
+            logging.info('%d: abs error %f, relative error %f',
+                         i, np.sqrt(aerr / n), rerr)
         i += 1
-        topk += inck
-        u, s, v = pypropack.svdp(y, k=topk, kmax=10 * topk)
-        while s[-1] > threshold:
-            topk += inck
-            u, s, v = pypropack.svdp(y, k=topk, kmax=10 * topk)
-        s = np.clip(s - threshold, 0, np.inf)
-        topk = len(s.nonzero()[0])
-        x = np.dot(u * s, v)
-        delta = w * (t - x)
-        err = np.linalg.norm(delta) / norm_t
-        logging.info('SVT %d: error %f using %d pcs [%s]',
-                     i, err, topk, ' '.join(str(int(x)) for x in s[:30]))
-        if err < tol:
-            break
-        y += learning_rate * delta
+    del t
+    del w
 
     def avg(xs):
         return np.mean(list(xs), axis=0)
@@ -164,7 +172,7 @@ def svt(df, threshold=None, tol=1e-3, learning_rate=1.5, window=10):
     def idx(a, b=None):
         return slice(df.index[a], df.index[b] if b else df.index[a])
 
-    parts = np.split(x, window, axis=1)
+    parts = np.split(np.dot(u.get_value(), v.get_value()), window, axis=1)
     w = window - 1
     f = num_frames - 1
 
@@ -180,8 +188,6 @@ def svt(df, threshold=None, tol=1e-3, learning_rate=1.5, window=10):
     for i in range(window):
         df.loc[idx(i), cols] = avg(parts[i - j][j] for j in range(i, -1, -1))
         df.loc[idx(f - i), cols] = avg(parts[w - (i - j + 1)][-j] for j in range(i+1, 0, -1))
-
-    return df
 
 
 def lowpass(df, freq=10., order=4):
@@ -209,31 +215,22 @@ def lowpass(df, freq=10., order=4):
     output='save smoothed data files to this directory tree',
     pattern=('process only trials matching this pattern', 'option'),
     frame_rate=('reindex frames to this rate', 'option', None, float),
-    tol=('fit SVT with this error tolerance', 'option', None, float),
+    tol=('fill dropouts with this error tolerance', 'option', None, float),
     window=('process windows of T frames', 'option', None, int),
     freq=('lowpass filter at N Hz', 'option', None, float),
 )
-def main(root, output, pattern='*', frame_rate=100, tol=0.02, window=30, freq=10):
-    trials = list(lmj.cubes.Experiment(root).trials_matching(pattern))
-    for t in trials:
+def main(root, output, pattern='*', frame_rate=100, tol=0.0001, window=20, freq=20):
+    for t in lmj.cubes.Experiment(root).trials_matching(pattern):
         t.load()
         t.mask_dropouts()
         t.reindex(frame_rate)
         for c in t.columns:
             if c.startswith('marker') and c[:-2] not in MARKERS:
                 del t.df[c]
-
-    # here we make a huge df containing all matching trial data.
-    keys = [(t.block.key, t.key) for t in trials]
-    df = pd.concat([t.df for t in trials], keys=keys)
-
-    # free up some memory.
-    [t.clear() for t in trials]
-
-    df = svt(df, tol=tol, window=window)
-
-    for t in trials:
-        t.df = df.loc[(t.block.key, t.key), :].copy()
+        try:
+            fill(t.df, tol=tol, window=window)
+        except Exception as e:
+            logging.exception('error filling dropouts!')
         lowpass(t.df, freq)
         t.save(t.root.replace(root, output))
 
