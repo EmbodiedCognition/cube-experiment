c55241c9efd09b1eb6f741717cf298fc944ee7a6
Leif Johnson <leif@lmjohns3.com>
2015-02-19 23:13:20 -0600

    Move reindexing code out of this script; use coordinate descent during optimization.

--- analysis/01-fill-dropouts.py
+++ analysis/01-fill-dropouts.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import division
+
 import climate
 import lmj.cubes
 import numpy as np
@@ -11,61 +13,8 @@ import theano.tensor as TT
 
 logging = climate.get_logger('fill')
 
-# this is the set of markers that gets included in our output.
-MARKERS = [
-    'marker00-r-head-back',
-    'marker01-r-head-front',
-    'marker02-l-head-front',
-    'marker03-l-head-back',
-    'marker06-r-collar',
-    'marker07-r-shoulder',
-    'marker08-r-elbow',
-    'marker09-r-wrist',
-    'marker12-r-fing-middle',
-    'marker13-r-fing-index',
-    'marker14-r-mc-outer',
-    'marker15-r-mc-inner',
-    'marker16-r-thumb-base',
-    'marker17-r-thumb-tip',
-    'marker18-l-collar',
-    'marker19-l-shoulder',
-    'marker20-l-elbow',
-    'marker21-l-wrist',
-    'marker24-l-fing-middle',
-    'marker25-l-fing-index',
-    'marker26-l-mc-outer',
-    'marker27-l-mc-inner',
-    'marker28-l-thumb-base',
-    'marker29-l-thumb-tip',
-    'marker30-abdomen',
-    'marker31-sternum',
-    'marker32-t3',
-    'marker33-t9',
-    'marker34-l-ilium',
-    'marker35-r-ilium',
-    'marker36-r-hip',
-    'marker37-r-knee',
-    'marker38-r-shin',
-    'marker39-r-ankle',
-    'marker40-r-heel',
-    'marker41-r-mt-outer',
-    'marker42-r-mt-inner',
-    'marker43-l-hip',
-    'marker44-l-knee',
-    'marker45-l-shin',
-    'marker46-l-ankle',
-    'marker47-l-heel',
-    'marker48-l-mt-outer',
-    'marker49-l-mt-inner',
-]
 
-
-def marker_channel_columns(df):
-    '''Return the names of columns that contain marker data.'''
-    return [c for c in df.columns if c[:-2] in MARKERS and c[-1] in 'xyz']
-
-
-def fill(df, tol=1e-4, window=20, gamma=1):
+def fill(df, tol=1e-4, window=5, gamma=1, rank=None):
     '''Complete missing marker data using matrix completion.
 
     This method alters the given `df` in-place.
@@ -78,12 +27,16 @@ def fill(df, tol=1e-4, window=20, gamma=1):
         Continue the reconstruction process until reconstructed data is within
         this relative tolerance threshold. Defaults to 1e-4.
     window : int, optional
-        Model windows of this many consecutive frames. Defaults to 20.
+        Model windows of this many consecutive frames. Defaults to 5.
     gamma : float, optional
         Use this parameter to regularize the low-rank entries on the factored
         data matrix. Defaults to 1.
+    rank : int, optional
+        Approximate the data using a matrix of this rank. Defaults to the
+        number of SVD components required to match the filled percentage of the
+        data.
     '''
-    cols = marker_channel_columns(df)
+    cols = [c for c in df.columns if c.startswith('marker') and c[-1] in 'xyz']
     data = df[cols]
     num_frames, num_channels = data.shape
     num_entries = num_frames * num_channels
@@ -94,7 +47,7 @@ def fill(df, tol=1e-4, window=20, gamma=1):
                  100 * filled_ratio)
 
     filled = data.fillna(0).values
-    weights = (~data.isnull()).values.astype(float)
+    weights = (~data.isnull()).values
 
     # here we create windows of consecutive data frames, all stacked together
     # along axis 1. for example, with 10 2-dimensional frames, we can stack them
@@ -116,56 +69,65 @@ def fill(df, tol=1e-4, window=20, gamma=1):
     # this stacked data matrix is the one we'll want to fill in the SVT process
     # below.
     w = np.concatenate([weights[i:num_frames-(window-1-i)] for i in range(window)], axis=1).astype(np.int8)
-    t = np.concatenate([filled[i:num_frames-(window-1-i)] for i in range(window)], axis=1).astype('f')
+    t = np.concatenate([filled[i:num_frames-(window-1-i)] for i in range(window)], axis=1).astype(np.float32)
     logging.info('processing windowed data %s', t.shape)
+    data_norm = (w * t * t).sum()
 
-    rank = 600
-    s = scipy.linalg.svd(t, compute_uv=False)
+    # compute one svd to initialize the optimization parameters below.
+    u, s, v = scipy.linalg.svd(t, full_matrices=False)
     cdf = (s / s.sum()).cumsum()
+    rank = rank or cdf.searchsorted(filled_ratio)
     logging.info('reconstructing with rank %d (%d%%) %s',
                  rank, 100 * cdf[rank],
                  [(f, cdf.searchsorted(f)) for f in (0.5, 0.8, 0.9, 0.99)])
 
     # optimization parameters
     gamma = TT.cast(gamma, 'float32')
-    learning_rate = TT.cast(0.0001, 'float32')
-    momentum = TT.cast(0.94, 'float32')
-    max_norm = TT.cast(100000, 'float32')
+    learning_rate = TT.cast(0.001, 'float32')
+    momentum = TT.cast(0.9, 'float32')
+    max_norm = TT.cast(10, 'float32')
     one = TT.cast(1, 'float32')
 
-    # symbolic variables.
-    data = TT.matrix('data')
-    mask = TT.bmatrix('mask')
+    # data variables.
+    data = theano.shared(t.astype('f'))
+    mask = theano.shared(w.astype('f'))
 
     # model parameters.
-    u = theano.shared(np.random.randn(t.shape[0], rank).astype('f'), name='u')
-    v = theano.shared(np.random.randn(rank, t.shape[1]).astype('f'), name='v')
+    ss = np.sqrt(np.clip(s[:rank] - s[int(np.sqrt(rank))], 0, 1e300))
+    u = theano.shared((u[:, :rank] * ss).astype('f'), name='u')
+    vu = theano.shared(np.zeros_like(u.get_value()), name='vu')
+    v = theano.shared((ss[:, None] * v[:rank]).astype('f'), name='v')
+    vv = theano.shared(np.zeros_like(v.get_value()), name='vv')
 
     # symbolic computations, including sgd with momentum.
+    def clip(g):
+        return g * TT.minimum(one, max_norm / TT.sqrt((g * g).sum()))
     err = data - TT.dot(u, v)
     sqerr = (mask * err * err).sum()
-    sqdat = (mask * data * data).sum()
     loss = sqerr + gamma * ((u * u).sum() + (v * v).sum())
-    updates = []
-    for param, grad in zip([u, v], TT.grad(loss, [u, v])):
-        g = grad * TT.minimum(one, max_norm / TT.sqrt((grad * grad).sum()))
-        v = theano.shared(np.zeros_like(param.get_value()), name='v' + param.name)
-        vnew = momentum * v - learning_rate * g
-        updates.append((v, vnew))
-        updates.append((param, param + vnew))
-    f = theano.function([data, mask], [sqerr, sqerr / sqdat], updates=updates)
+    gu, gv = TT.grad(loss, [u, v])
+    velu = momentum * vu - learning_rate * clip(gu)
+    velv = momentum * vv - learning_rate * clip(gv)
+    fu = theano.function([], [sqerr, gu], updates=[(vu, velu), (u, u + velu)])
+    fv = theano.function([], [sqerr, gv], updates=[(vv, velv), (v, v + velv)])
 
     i = 0
-    n = w.sum()
-    rerr = tol + 1
-    while rerr > tol:
-        aerr, rerr = f(t, w)
+    err = 1e200
+    prev_err = 1e300
+    while tol * data_norm < err < prev_err:
+        prev_err = err
+        err, gu = fu()
+        err, gv = fv()
         if not i % 10:
-            logging.info('%d: abs error %f, relative error %f',
-                         i, np.sqrt(aerr / n), rerr)
+            r = w * (t - np.dot(u.get_value(), v.get_value()))
+            logging.info('%d: error %f (sanity: %f; %f mean; %s) gu %.1f gv %.1f',
+                         i, err / data_norm,
+                         (r * r).sum() / data_norm,
+                         abs(r).mean(),
+                         np.percentile(abs(r), [50, 90, 95, 99]).round(4),
+                         np.sqrt((gu * gu).sum()),
+                         np.sqrt((gv * gv).sum()))
         i += 1
-    del t
-    del w
 
     def avg(xs):
         return np.mean(list(xs), axis=0)
@@ -207,29 +169,26 @@ def lowpass(df, freq=10., order=4):
     nyquist = 1 / (2 * pd.Series(df.index).diff().mean())
     assert 0 < freq < nyquist
     b, a = scipy.signal.butter(order, freq / nyquist)
-    for c in marker_channel_columns(df):
-        df.loc[:, c] = scipy.signal.filtfilt(b, a, df[c])
+    for c in df.columns:
+        if c.startswith('marker') and c[-1] in 'xyz':
+            df.loc[:, c] = scipy.signal.filtfilt(b, a, df[c])
 
 
 @climate.annotate(
     root='load data files from this directory tree',
     output='save smoothed data files to this directory tree',
     pattern=('process only trials matching this pattern', 'option'),
-    frame_rate=('reindex frames to this rate', 'option', None, float),
     tol=('fill dropouts with this error tolerance', 'option', None, float),
     window=('process windows of T frames', 'option', None, int),
+    rank=('reconstruct data using matrix of this rank', 'option', None, int),
     freq=('lowpass filter at N Hz', 'option', None, float),
 )
-def main(root, output, pattern='*', frame_rate=100, tol=0.0001, window=20, freq=None):
+def main(root, output, pattern='*', tol=0.0001, window=5, rank=400, freq=20):
     for t in lmj.cubes.Experiment(root).trials_matching(pattern):
         t.load()
         t.mask_dropouts()
-        t.reindex(frame_rate)
-        for c in t.columns:
-            if c.startswith('marker') and c[:-2] not in MARKERS:
-                del t.df[c]
         try:
-            fill(t.df, tol=tol, window=window)
+            fill(t.df, tol=tol, window=window, rank=rank)
         except Exception as e:
             logging.exception('error filling dropouts!')
         if freq:
