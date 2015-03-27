3135c931a092cd5dab88f5eee7a4a5f96a5d94cc
Leif Johnson <leif@lmjohns3.com>
2015-03-26 21:17:59 -0500

    Run an entire subject at once.

--- analysis/03-fill-dropouts.sh
+++ analysis/03-fill-dropouts.sh
@@ -6,44 +6,17 @@ function job {
     python analysis/03-fill-dropouts.py $args -pattern "$1/*block0[$2]/"
 }
 
-job f9 01
-job f9 23
-
-job 68 01
-job 68 23
-
-job ca 01
-job ca 23
-job ca 04
-
-job d1 02
-job d1 03
-job d1 14
-
-job 85 02
-job 85 13
-job 85 04
-
-job af 01
-job af 23
-job af 45
-
-job 16 01
-job 16 23
-job 16 45
-
-job 65 02
-job 65 13
-job 65 45
-
-job 14 01
-job 14 23
-job 14 04
-
-job c9 01
-job c9 23
-job c9 04
-
-job 3b 01
-job 3b 23
-job 3b 04
+job f9 0123
+job 68 0123
+job ca 01234
+job d1 01234
+job 85 01234
+job af 012
+job af 345
+job 16 012
+job 16 345
+job 65 012
+job 65 345
+job 14 01234
+job c9 01234
+job 3b 01234
