e8a66a13916e333e11518836413f927d04c8d06c
Leif Johnson <leif@lmjohns3.com>
2015-04-08 17:27:05 -0500

    Move to use common code from fill module.

--- analysis/03-fill-dropouts-autoencoder.py
+++ analysis/03-fill-dropouts-autoencoder.py
@@ -4,9 +4,8 @@ from __future__ import division
 
 import climate
 import lmj.cubes
+import lmj.cubes.fill
 import numpy as np
-import pandas as pd
-import scipy.signal
 import theanets
 import theanets.flags
 
@@ -20,9 +19,9 @@ def fill(dfs, rank, tol, window):
     Parameters
     ----------
     dfs : list of pd.DataFrame
-        Data frames for source data. Each frame will be interpolated, and then
-        the frames will be stacked into a single large frame to use during
-        encoding. This stacked frame will then be split and returned.
+        Frames of source data. The frames will be stacked into a single large
+        frame to use during encoding. This stacked frame will then be split and
+        returned.
     rank : int
         Encode the data using a nonlinear matrix decomposition of this rank.
     tol : float
@@ -31,95 +30,29 @@ def fill(dfs, rank, tol, window):
     window : int
         Model windows of this many consecutive frames.
     '''
-    assert window > 1
-
-    cols = [c for c in dfs[0].columns if c.startswith('marker') and c[-1] in 'xyz']
-    pad = pd.DataFrame(float('nan'), index=list(range(window - 1)), columns=cols)
-    chunks, keys = [pad], [-1]
-    for i, df in enumerate(dfs):
-        chunks.extend([df[cols], pad])
-        keys.extend([i, -len(chunks)])
-    data = pd.concat(chunks, axis=0, keys=keys)
-
-    num_frames, num_channels = data.shape
-    num_entries = num_frames * num_channels
-    filled_ratio = data.count().sum() / num_entries
-    logging.info('missing %d of (%d, %d) = %d values (%.1f%% filled)',
-                 num_entries - data.count().sum(),
-                 num_frames, num_channels, num_entries,
-                 100 * filled_ratio)
-
-    # if a column is completely missing, refuse to process the data.
-    for c in cols:
-        if data[c].count() == 0:
-            raise ValueError('%s: no visible values!', c)
-
-    weights = (~data.isnull()).values.astype('f')
-    filled = data.fillna(0).values.astype('f')
-
-    # here we create windows of consecutive data frames, all stacked together
-    # along axis 1. for example, with 10 2-dimensional frames, we can stack them
-    # into windows of length 4 as follows:
-    #
-    #     data   windows
-    # 0   A B    A B C D E F G H
-    # 1   C D    C D E F G H I J
-    # 2   E F    E F G H I J K L
-    # 3   G H    G H I J K L M N
-    # 4   I J    I J K L M N O P
-    # 5   K L    K L M N O P Q R
-    # 6   M N    M N O P Q R S T
-    # 7   O P
-    # 8   Q R
-    # 9   S T
-    #
-    # this is more or less like a convolution with a sliding rectangular window.
-    # this stacked data matrix is the one we'll want to fill in the encoding
-    # process below.
-    pos = np.concatenate([filled[i:num_frames-(window-1-i)] for i in range(window)], axis=1)
-    wgt = np.concatenate([weights[i:num_frames-(window-1-i)] for i in range(window)], axis=1)
-
-    data_norm = (wgt * pos * pos).sum()
-    logging.info('processing windowed data %s: norm %s', pos.shape, data_norm)
-    assert np.isfinite(data_norm)
+    df = lmj.cubes.fill.stack(dfs, window)
+    centers = lmj.cubes.fill.center(df)
+    pos, vis, _ = lmj.cubes.fill.window(df, window)
 
     exp = theanets.Experiment(
         theanets.Autoencoder,
-        layers=(pos.shape[1], dict(size=rank, sparsity=0.99), pos.shape[1]),
+        layers=(pos.shape[1], rank, pos.shape[1]),
         weighted=True)
-    exp.enable_command_line()
-    exp.train([pos, wgt], learning_rate=0.1, patience=1)
-    exp.train([pos, wgt], learning_rate=0.01, patience=1)
-    for tm, _ in exp.itertrain([pos, wgt]):
+    exp.train([pos, vis], learning_rate=0.1, patience=1)
+    for tm, _ in exp.itertrain([pos, vis]):
         if tm['loss'] < tol:
             break
 
-    w = window - 1
-    rows = pd.MultiIndex(levels=data.index.levels,
-                         labels=[x[w:-w] for x in data.index.labels])
     batches = (exp.network.predict(pos[o:o+64]) for o in range(0, len(pos), 64))
-    parts = np.split(np.concatenate(list(batches), axis=0), window, axis=1)
-    aligned = [p[w - j:num_frames - w - j] for j, p in enumerate(parts)]
-    filled = pd.DataFrame(np.mean(aligned, axis=0), index=rows, columns=cols)
-    data.update(data.loc[rows, cols].fillna(filled))
-
-    # unstack the stacked data frame.
-    for i, df in enumerate(dfs):
-        df[cols] = data.loc[(i, ), cols]
+    lmj.cubes.fill.update(df, np.concatenate(list(batches), axis=0), window)
+    lmj.cubes.fill.restore(df, centers)
+    lmj.cubes.fill.unstack(df, dfs)
 
 
 def main(args):
-    for _, ts in lmj.cubes.Experiment(args.root).by_subject(args.pattern):
-        ts = list(ts)
-        for t in ts:
-            t.load()
-            t.mask_dropouts()
-        try:
-            fill([t.df for t in ts], args.rank, args.tol, args.window)
-        except Exception as e:
-            logging.exception('error filling dropouts!')
-            continue
-        [t.save(t.root.replace(args.root, args.output)) for t in ts]
+    lmj.cubes.fill.main(
+        args, lambda ts: fill(
+            [t.df for t in ts], args.rank, args.tol, args.window))
 
 
 if __name__ == '__main__':
--- analysis/03-fill-dropouts-svt.py
+++ analysis/03-fill-dropouts-svt.py
@@ -4,166 +4,66 @@ from __future__ import division
 
 import climate
 import lmj.cubes
+import lmj.cubes.fill
 import numpy as np
 import pandas as pd
-import scipy.signal
 
 logging = climate.get_logger('fill')
 
-CENTERS = [
-    'marker34-l-ilium',
-    'marker35-r-ilium',
-    'marker36-r-hip',
-    'marker37-r-knee',
-]
-
-def svt(dfs, tol=1e-4, threshold=None, window=5):
+def svt(dfs, tol, threshold, window):
     '''Complete missing marker data using singular value thresholding.
 
-    This method alters the given `df` in-place.
+    This method alters the given `dfs` in-place.
 
     Parameters
     ----------
     dfs : list of pd.DataFrame
-        Data frames for source data. Each frame will be interpolated, and then
-        the frames will be stacked into a single large frame to use during SVT.
-        This stacked frame will then be split and returned.
-    tol : float, optional
+        Frames of source data. The frames will be stacked into a single large
+        frame to use during SVT. This stacked frame will then be split and
+        returned.
+    tol : float
         Continue the reconstruction process until reconstructed data is within
-        this relative tolerance threshold. Defaults to 1e-4.
-    threshold : float, optional
-        Threshold for singular values. Defaults to a value computed from the
+        this relative tolerance threshold.
+    threshold : float
+        Threshold for singular values. If none, use a value computed from the
         spectrum of singular values.
-    window : int, optional
-        Model windows of this many consecutive frames. Defaults to 5.
-
-    Returns
-    -------
-    dfs : list of pd.DataFrame
-        A list of data frames containing data with dropouts filled in.
+    window : int
+        Model windows of this many consecutive frames.
     '''
-    assert window > 1
-
-    cols = [c for c in dfs[0].columns if c.startswith('marker') and c[-1] in 'xyz']
-    pad = pd.DataFrame(float('nan'), index=list(range(window - 1)), columns=cols)
-    chunks, keys = [pad], [-1]
-    for i, df in enumerate(dfs):
-        chunks.extend([df[cols], pad])
-        keys.extend([i, -len(chunks)])
-    data = pd.concat(chunks, axis=0, keys=keys)
-
-    num_frames, num_channels = data.shape
-    num_entries = num_frames * num_channels
-    filled_ratio = data.count().sum() / num_entries
-    logging.info('missing %d of (%d, %d) = %d values (%.1f%% filled)',
-                 num_entries - data.count().sum(),
-                 num_frames, num_channels, num_entries,
-                 100 * filled_ratio)
-
-    # if a column is completely missing, refuse to process the data.
-    for c in cols:
-        if data[c].count() == 0:
-            raise ValueError('%s: no visible values!', c)
-
-    # interpolate dropouts linearly.
-    filled = data.interpolate().ffill().bfill()
-
-    # shift the entire mocap recording by the location of the CENTERS (basically
-    # the hip markers).
-    center = pd.DataFrame(
-        dict(x=filled[[m + '-x' for m in CENTERS]].mean(axis=1),
-             y=filled[[m + '-y' for m in CENTERS]].mean(axis=1),
-             z=filled[[m + '-z' for m in CENTERS]].mean(axis=1)))
-    for c in cols:
-        filled[c] -= center[c[-1]]
-
-    filled = filled.values
-    weights = (~data.isnull()).values
-
-    # here we create windows of consecutive data frames, all stacked together
-    # along axis 1. for example, with 10 2-dimensional frames, we can stack them
-    # into windows of length 4 as follows:
-    #
-    #     data   windows
-    # 0   A B    A B C D E F G H
-    # 1   C D    C D E F G H I J
-    # 2   E F    E F G H I J K L
-    # 3   G H    G H I J K L M N
-    # 4   I J    I J K L M N O P
-    # 5   K L    K L M N O P Q R
-    # 6   M N    M N O P Q R S T
-    # 7   O P
-    # 8   Q R
-    # 9   S T
-    #
-    # this is more or less like a convolution with a sliding rectangular window.
-    # this stacked data matrix is the one we'll want to fill in the SVT process
-    # below.
-    w = np.concatenate([weights[i:num_frames-(window-1-i)] for i in range(window)], axis=1)
-    t = np.concatenate([filled[i:num_frames-(window-1-i)] for i in range(window)], axis=1)
-    data_norm = (w * t * t).sum()
-    logging.info('processing windowed data %s: norm %s', t.shape, data_norm)
-
-    assert np.isfinite(data_norm)
+    df = lmj.cubes.fill.stack(dfs, window)
+    centers = lmj.cubes.fill.center(df)
+    pos, vis, data_norm = lmj.cubes.fill.window(df, window)
 
     # if the threshold is none, set it using the falloff point in the spectrum.
     if threshold is None:
-        s = pd.Series(np.linalg.svd(t, compute_uv=False))
+        s = pd.Series(np.linalg.svd(pos, compute_uv=False))
         threshold = s[s.diff().diff().shift(-1).argmax()]
         logging.info('using threshold %.2f', threshold)
 
     i = 0
     err = 1e200
-    x = y = np.zeros_like(t)
+    x = y = np.zeros_like(pos)
     while tol * data_norm < err:
-        e = w * (t - x)
+        e = vis * (pos - x)
         err = (e * e).sum()
         y += e
         u, s, v = np.linalg.svd(y, full_matrices=False)
         s = np.clip(s - threshold, 0, np.inf)
         x = np.dot(u * s, v)
         logging.info('%d: error %f (%d); mean %f; %s',
-                     i, err / data_norm, len(s.nonzero()[0]), abs(e[w]).mean(),
-                     np.percentile(abs(e[w]), [50, 90, 95, 99]).round(4))
+                     i, err / data_norm, len(s.nonzero()[0]), abs(e[vis]).mean(),
+                     np.percentile(abs(e[vis]), [50, 90, 95, 99]).round(4))
         i += 1
 
-    def avg(xs):
-        return np.mean(list(xs), axis=0)
-
-    parts = np.split(x, window, axis=1)
-    w = window - 1
-
-    # above, we created <window> duplicates of our data, each offset by 1 frame,
-    # and stacked (along axis 1) into a big ol matrix. in effect, we have
-    # <window> copies of each frame; here, we unpack these duplicates, remove
-    # their offsets, take the average, and put them back in the linear frame.
-    rows = pd.MultiIndex(levels=data.index.levels,
-                         labels=[x[w:-w] for x in data.index.labels])
-    data.loc[rows, cols] = avg(
-        parts[j][w - j:num_frames - w - j] for j in range(window))
-
-    # move our interpolated data back out to the world by restoring the
-    # locations of the CENTERS.
-    for c in cols:
-        data.loc[:, c] += center.loc[:, c[-1]]
-
-    # unstack the stacked data frame.
-    for i, df in enumerate(dfs):
-        df[cols] = data.loc[(i, ), cols]
+    lmj.cubes.fill.update(df, x, window)
+    lmj.cubes.fill.restore(df, centers)
+    lmj.cubes.fill.unstack(df, dfs)
 
 
 def main(args):
-    for _, ts in lmj.cubes.Experiment(args.root).by_subject(args.pattern):
-        ts = list(ts)
-        for t in ts:
-            t.load()
-            t.mask_dropouts()
-        try:
-            svt([t.df for t in ts], args.tol, args.threshold, args.window)
-        except Exception as e:
-            logging.exception('error filling dropouts!')
-            continue
-        [t.save(t.root.replace(args.root, args.output)) for t in ts]
+    lmj.cubes.fill.main(
+        args, lambda ts: svt(
+            [t.df for t in ts], args.tol, args.threshold, args.window))
 
 
 if __name__ == '__main__':
