01c74cfea946eac098a0e144380314cd4676cf2f
Leif Johnson <leif@lmjohns3.com>
2015-04-08 16:15:39 -0500

    Split lowpass filtering into another script.

---
+++ analysis/04-lowpass.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from __future__ import division
+
+import climate
+import lmj.cubes
+import pandas as pd
+import scipy.signal
+
+logging = climate.get_logger('lowpass')
+
+def lowpass(df, freq=10., order=4):
+    '''Filter marker data using a butterworth low-pass filter.
+
+    This method alters the data in `df` in-place.
+
+    Parameters
+    ----------
+    freq : float, optional
+        Use a butterworth filter with this cutoff frequency. Defaults to
+        10Hz.
+    order : int, optional
+        Order of the butterworth filter. Defaults to 4.
+    '''
+    nyquist = 1 / (2 * pd.Series(df.index).diff().mean())
+    assert 0 < freq < nyquist
+    passes = 2  # filtfilt makes two passes over the data.
+    correct = (2 ** (1 / passes) - 1) ** 0.25
+    b, a = scipy.signal.butter(order / passes, (freq / correct) / nyquist)
+    for c in df.columns:
+        if c.startswith('marker') and c[-1] in 'xyz':
+            df.loc[:, c] = scipy.signal.filtfilt(b, a, df[c])
+
+
+@climate.annotate(
+    root='load data files from this directory tree',
+    output='save smoothed data files to this directory tree',
+    pattern=('process only trials matching this pattern', 'option'),
+    freq=('lowpass filter at N Hz', 'option', None, float),
+)
+def main(root, output, pattern='*', freq=None):
+    for t in lmj.cubes.Experiment(root).trials_matching(pattern):
+        lowpass(t.df, freq)
+        t.save(t.root.replace(root, output))
+
+
+if __name__ == '__main__':
+    climate.call(main)
