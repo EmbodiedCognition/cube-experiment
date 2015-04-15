2ee1c31fb64bfade96c262064e8d6810cf5b005b
Leif Johnson <leif@lmjohns3.com>
2015-04-15 12:59:10 -0500

    Add plotting method.

--- analysis/inspect-trial.py
+++ analysis/inspect-trial.py
@@ -8,6 +8,14 @@ import numpy as np
 import pandas as pd
 
 
+def hist(trial, cols, bins=None):
+    if bins is None:
+        bins = np.linspace(-1000, 1000, 127)
+    values = trial.df[cols].values.flatten()
+    weights = np.ones_like(values) / len(values)
+    lmj.plot.hist(values, bins=bins, lw=2, log=True, weights=weights, histtype='step')
+
+
 def main(root, pattern):
     for trial in lmj.cubes.Experiment(root).trials_matching(pattern):
         trial.load()
