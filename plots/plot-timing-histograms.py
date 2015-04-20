396079f6c0f1eb99df02def89ea43876b0e08615
Leif Johnson <leif@lmjohns3.com>
2015-04-19 20:04:27 -0500

    Tweak graph.

--- plots/plot-timing-histograms.py
+++ plots/plot-timing-histograms.py
@@ -15,12 +15,12 @@ def diffs(t):
 def main(root, pattern='*'):
     trials = lmj.cubes.Experiment(root).trials_matching(pattern)
     values = joblib.Parallel(-1)(joblib.delayed(diffs)(t) for t in trials)
-    values = [x for xs in values for x in xs]
+    #values = [x for xs in values for x in xs]
     with lmj.plot.axes(spines=True, max_xticks=6, max_yticks=6) as ax:
         ax.hist(values, bins=np.linspace(5, 15, 127), lw=0, stacked=True)
         ax.set_xlim(5, 15)
-        ax.set_xlabel('Time Between Frames (msec)')
-        ax.set_ylabel('Number of Observations')
+        ax.set_xlabel('Frame Duration (msec)')
+        ax.set_ylabel('Number of Frames')
 
 
 if __name__ == '__main__':
