1ab394173cb19310bc07fdb4b680a50ff2ff2c0d
Leif Johnson <leif@lmjohns3.com>
2015-07-26 13:11:51 -0500

    Fix all-NaNs issue with reindexing.
    Remove more markers from the raw data.

--- analysis/02-reindex.py
+++ analysis/02-reindex.py
@@ -16,47 +16,47 @@ MARKERS = [
     'marker03-l-head-back',
     #'marker04-r-head-mid',
     #'marker05-l-head-mid',
-    'marker06-r-collar',
+    #'marker06-r-collar',
     'marker07-r-shoulder',
-    'marker08-r-elbow',
-    'marker09-r-wrist',
+    #'marker08-r-elbow',
+    #'marker09-r-wrist',
     #'marker10-r-fing-pinky',
     #'marker11-r-fing-ring',
     #'marker12-r-fing-middle',
     'marker13-r-fing-index',
     'marker14-r-mc-outer',
     #'marker15-r-mc-inner',
-    'marker16-r-thumb-base',
+    #'marker16-r-thumb-base',
     #'marker17-r-thumb-tip',
-    'marker18-l-collar',
+    #'marker18-l-collar',
     'marker19-l-shoulder',
-    'marker20-l-elbow',
-    'marker21-l-wrist',
+    #'marker20-l-elbow',
+    #'marker21-l-wrist',
     #'marker22-l-fing-pinky',
     #'marker23-l-fing-ring',
     #'marker24-l-fing-middle',
     'marker25-l-fing-index',
     'marker26-l-mc-outer',
     #'marker27-l-mc-inner',
-    'marker28-l-thumb-base',
+    #'marker28-l-thumb-base',
     #'marker29-l-thumb-tip',
-    'marker30-abdomen',
+    #'marker30-abdomen',
     'marker31-sternum',
-    'marker32-t3',
-    'marker33-t9',
+    #'marker32-t3',
+    #'marker33-t9',
     'marker34-l-ilium',
     'marker35-r-ilium',
     'marker36-r-hip',
-    'marker37-r-knee',
-    'marker38-r-shin',
-    'marker39-r-ankle',
+    #'marker37-r-knee',
+    #'marker38-r-shin',
+    #'marker39-r-ankle',
     'marker40-r-heel',
     'marker41-r-mt-outer',
     'marker42-r-mt-inner',
     'marker43-l-hip',
-    'marker44-l-knee',
-    'marker45-l-shin',
-    'marker46-l-ankle',
+    #'marker44-l-knee',
+    #'marker45-l-shin',
+    #'marker46-l-ankle',
     'marker47-l-heel',
     'marker48-l-mt-outer',
     'marker49-l-mt-inner',
@@ -67,6 +67,13 @@ ACCELS = (0.5, 1, 2, 5, 10, 20, 50, 100, 200)
 
 def reindex(t, root, output, frame_rate, max_speed, max_acc):
     t.load()
+
+    # drop marker data not in MARKERS.
+    for c in set(t.marker_columns) - set(MARKERS):
+        del t.df[c + '-x']
+        del t.df[c + '-y']
+        del t.df[c + '-z']
+        del t.df[c + '-c']
 
     def below(s, r):
         return '{}<{}'.format((s < r).sum(), r)
@@ -88,25 +95,28 @@ def reindex(t, root, output, frame_rate, max_speed, max_acc):
               (acc > max_acc).sum(), ' '.join(below(acc, r) for r in ACCELS))
         t.df.ix[mask, marker + '-x':marker + '-c'] = float('nan')
 
-    # drop unusable marker data.
-    for c in t.columns:
-        if c.startswith('marker') and c[:-2] not in MARKERS:
-            del t.df[c]
-
-    # reindex to regularly-spaced temporal index values.
+    # fill nans locally before reindexing.
     limit = int(np.ceil(100 / frame_rate))
     for c in t.df.columns:
+        s, n = t.df[c], None
         if not c.startswith('marker'):
-            t.df[c] = t.df[c].bfill().ffill()
+            # fill all nans in non-marker columns (e.g., source, target).
+            n = s.bfill().ffill()
         elif c[-1] in 'xyz':
-            fw = t.df[c].interpolate(method='values', limit=limit)
-            bw = t.df[c][::-1].interpolate(method='values', limit=limit)
-            t.df[c] = fw.fillna(bw[::-1])
+            # interpolate nearby position data.
+            fw = s.interpolate(method='values', limit=limit)
+            bw = s[::-1].interpolate(method='values', limit=limit)
+            n = fw.fillna(bw[::-1])
         elif c[-1] in 'c':
-            fw = t.df[c].fillna(value=1.2345, limit=limit)
-            bw = t.df[c][::-1].fillna(value=1.2345, limit=limit)
-            t.df[c] = fw.fillna(bw[::-1])
-    t.df = t.df.reindex(np.arange(0, t.df.index[-1], 1 / frame_rate))
+            # fill nearby condition values.
+            fw = s.fillna(value=1.2345, limit=limit)
+            bw = s[::-1].fillna(value=1.2345, limit=limit)
+            n = fw.fillna(bw[::-1])
+        t.df[c] = n
+
+    # reindex to regularly-spaced temporal index values.
+    t.df = t.df.reindex(np.arange(0, t.df.index[-1], 1 / frame_rate),
+                        method='nearest', limit=1)
 
     t.save(t.root.replace(root, output))
 
@@ -121,8 +131,8 @@ def reindex(t, root, output, frame_rate, max_speed, max_acc):
 )
 def main(root, output, pattern='*', frame_rate=100, max_speed=5, max_acc=100):
     trials = lmj.cubes.Experiment(root).trials_matching(pattern)
-    work = joblib.delayed(reindex)
     args = root, output, frame_rate, max_speed, max_acc
+    work = joblib.delayed(reindex)
     joblib.Parallel(-1)(work(t, *args) for t in trials)
 
 
