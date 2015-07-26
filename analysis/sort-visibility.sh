af7fe6385e30ee07c66b6efad31fd38809666d64
Leif Johnson <leif@lmjohns3.com>
2015-07-26 13:31:42 -0500

    Fix default variable values.

--- analysis/sort-visibility.sh
+++ analysis/sort-visibility.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-f=${1=/data/cubes/visibility-05ms-050mss-all.tsv}
+file=${1:-/data/cubes/visibility-05ms-050mss-all.tsv}
+col=${2:-7}
 
-sort -n -k7 <(sed '1d' $f) \
+echo $col $file
+
+head -1 $file | sed 's|marker|\tmarker\t|'
+sed '1d' $file | sort -n -k$col \
     | sed 's,\(r-hip\|r-shin\|l-shin\|l-hip\|r-knee\|l-knee\|r-heel\|l-heel\|t3\|t9\),\1\t,' \
     | sed 's|\([0-9]\{3\}\)[0-9]*|\1|g'
