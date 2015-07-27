99137e337e0bc8f5dc21d7b4cef8a84054a4de68
Leif Johnson <leif@lmjohns3.com>
2015-07-26 19:34:12 -0500

    Fix imports, save goal-relative data too.

--- analysis/10-extract-jacobian-chunks.py
+++ analysis/10-extract-jacobian-chunks.py
@@ -1,6 +1,9 @@
 import climate
 import joblib
+import lmj.cubes
+import logging
 import numpy as np
+import os
 
 
 def extract(trial, output, frames):
@@ -24,15 +27,28 @@ def extract(trial, output, frames):
     body.add_velocities()
     body = body.df[body.marker_channel_columns]
 
+    goal = lmj.cubes.Trial(trial.parent, trial.basename)
+    goal.df = trial.df.copy()
+    goal.make_target_relative()
+    goal.add_velocities()
+    goal = goal.df[goal.marker_channel_columns]
+
     _, jac = trial.jacobian(frames)
 
     start = frames
-    for i, end in enumerate(trial.df.target.diff(1).nonzero()[0][1:]):
+    starts = list(trial.df.target.diff(1).nonzero()[0])
+    for i, end in enumerate(starts[1:] + [len(body)]):
         save('body-{:02d}'.format(i), body.iloc[start:end])
+        save('goal-{:02d}'.format(i), goal.iloc[start:end])
         save('jac-{:02d}'.format(i), jac.iloc[start:end])
         start = end + frames
 
 
+@climate.annotate(
+    root='load data from this root directory',
+    output='save chunks to this directory',
+    frames=('compute jacobian over this many frames', 'option', None, int),
+)
 def main(root, output, frames=10):
     trials = lmj.cubes.Experiment(root).trials_matching('*')
     work = joblib.delayed(extract)
