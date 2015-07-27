96b5b5c318f5fba59ad243fe52933cd30e7a1257
Leif Johnson <leif@lmjohns3.com>
2015-07-26 19:06:26 -0500

    Fill dropouts on each subject in one go.

--- analysis/03-fill-dropouts.sh
+++ analysis/03-fill-dropouts.sh
@@ -13,12 +13,9 @@ job 68 0123
 job ca 01234
 job d1 01234
 job 85 01234
-job af 012
-job af 345
-job 16 012
-job 16 345
-job 65 012
-job 65 345
+job af 012345
+job 16 012345
+job 65 012345
 job 14 01234
 job c9 01234
 job 3b 01234
