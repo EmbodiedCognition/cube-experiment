2c0110e0e58165f55dd3cb4f69654d215629ad66
Leif Johnson <leif@lmjohns3.com>
2015-08-03 10:54:20 -0500

    Preserve all info in body and goal relative chunks

--- analysis/10-extract-jacobian-chunks.py
+++ analysis/10-extract-jacobian-chunks.py
@@ -8,6 +8,9 @@ import numpy as np
 import os
 
 
+TARGETS = '0123456789ab'
+
+
 def extract(trial, output, frames):
     dirname = os.path.join(output, trial.subject.key, trial.block.key)
 
@@ -28,24 +31,22 @@ def extract(trial, output, frames):
     body.df = trial.df.copy()
     body.make_body_relative()
     body.add_velocities()
-    body = body.df[body.marker_channel_columns]
 
     goal = lmj.cubes.Trial(trial.parent, trial.basename)
     goal.df = trial.df.copy()
     goal.make_target_relative()
     goal.add_velocities()
-    goal = goal.df[goal.marker_channel_columns]
 
     _, jac = trial.jacobian(frames)
 
-    for t, target in enumerate('0123456789ab'):
+    for t, target in enumerate(TARGETS):
         mask = trial.df.target == t
         if np.sum(mask) > 0:
             sources = trial.df[mask].source.unique()
             assert len(sources) == 1
-            tgt = '{}{}'.format('0123456789ab'[int(sources[0])], target)
-            save(body[mask], tgt, 'body')
-            save(goal[mask], tgt, 'goal')
+            tgt = '{}{}'.format(TARGETS[int(sources[0])], target)
+            save(body.df[mask], tgt, 'body')
+            save(goal.df[mask], tgt, 'goal')
             save(jac[mask], tgt, 'jac')
 
 
