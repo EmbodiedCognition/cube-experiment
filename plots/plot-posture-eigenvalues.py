73ef78a210a1a7b7c96f1d2a12c23cc4863c7e25
Leif Johnson <leif@lmjohns3.com>
2015-09-20 14:33:41 -0500

    Update eigenvalues plot (still outdated).

--- plots/plot-posture-eigenvalues.py
+++ plots/plot-posture-eigenvalues.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
 
 import climate
+import lmj.cubes
 import lmj.pca
 import lmj.plot
+import numpy as np
+import pandas as pd
 
 
-def main(root, pca):
+@climate.annotate(
+    pca='load PCA from this file on disk',
+    root='load experiment data from this directory tree',
+    pattern=('load trials matching this pattern', 'option'),
+    output=('save movie in this output filename', 'option'),
+)
+def main(pca, root, pattern='*', output=None):
+    trial = next(lmj.cubes.Experiment(root).trials_matching(pattern))
+    trial.load()
+    trial.add_velocities()
+
     pca = lmj.pca.PCA(filename=pca)
 
+    '''
     K = 100
     annkw = dict(
         color='#d62728',
@@ -27,6 +41,17 @@ def main(root, pca):
             s = '{:.0f}%'.format(100 * c)
             ax.axvline(k, 0, pca.vals[0], color='#d62728', lw=1)
             ax.annotate(s, xy=(k, 0.26), xytext=(k + 2, 0.28), **annkw)
+    '''
+
+    mov = lmj.cubes.Movement(pd.DataFrame(
+        (pca.vals * pca.vecs).T, columns=trial.marker_channel_columns))
+    def render(ax):
+        lmj.cubes.plots.skeleton(ax, mov, 10, lw=2)
+    anim = lmj.plot.rotate_3d(
+        lmj.cubes.plots.show_3d(render),
+        output=output, azim=(0, 90), elev=(5, 20), fig=dict(figsize=(10, 4.8)))
+    if not output:
+        lmj.plot.show()
 
 
 if __name__ == '__main__':
