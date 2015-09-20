57eed04884ab450c4bbdbb6dc0023ce4b3c81d92
Leif Johnson <leif@lmjohns3.com>
2015-09-20 15:02:59 -0500

    Get GEM plots working!

--- plots/goal-equivalent-manifold.py
+++ plots/goal-equivalent-manifold.py
@@ -7,24 +7,24 @@ import lmj.cubes
     root='plot data rooted at this path',
     pattern=('plot data from files matching this pattern', 'option'),
     targets=('only plot these targets', 'option'),
-    output=('save movie in this output filename', 'option'),
 )
-def main(root, pattern='*/*block01/*trial01*.csv.gz', targets=None, output=None):
-    targets = set(range(12))
+def main(root, pattern='*', targets=None):
+    ts = set(range(12))
     if targets is not None:
-        targets = set(int(x.strip()) for x in targets.strip().split(','))
+        ts = set(int(x.strip()) for x in targets.strip().split(','))
+    targets = ts
     with lmj.cubes.plots.space() as ax:
         cubes = True
-        for t in lmj.cubes.Experiment(root).trials_matching(pattern):
-            t.load()
-            t.add_velocities(smooth=7)
+        for trial in lmj.cubes.Experiment(root).trials_matching(pattern):
+            trial.load()
+            trial.add_velocities(smooth=7)
             if cubes:
-                lmj.cubes.plots.show_cubes(ax, t)
+                lmj.cubes.plots.show_cubes(ax, trial)
                 cubes = False
-            for T in targets:
-                mov = t.movement_to(T)
+            for target in targets:
+                mov = trial.movement_to(target)
                 if len(mov.df):
-                    lmj.cubes.plots.skeleton(ax, mov, -1, lw=2)
+                    lmj.cubes.plots.skeleton(ax, mov, -1, lw=2, alpha=0.1)
 
 
 if __name__ == '__main__':
