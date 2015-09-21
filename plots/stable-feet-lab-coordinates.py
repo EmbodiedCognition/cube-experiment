9655a7815f02b944579cbad8859683004333002e
Leif Johnson <leif@lmjohns3.com>
2015-09-20 22:57:11 -0500

    Tweak.

--- plots/stable-feet-lab-coordinates.py
+++ plots/stable-feet-lab-coordinates.py
@@ -9,7 +9,7 @@ import utils
 def load(root, pattern):
     for trial in lmj.cubes.Experiment(root).trials_matching(pattern):
         trial.load()
-        trial.add_velocities(19)
+        trial.add_velocities(13)
         l = trial.trajectory(47, velocity=True)
         l['speed'] = np.sqrt((l[['vx', 'vy', 'vz']] ** 2).sum(axis=1))
         lm = utils.local_minima(l.speed.values)
@@ -44,8 +44,8 @@ def main(root, pattern='*'):
                 x.extend(right.x.values[ri:])
                 z.extend(right.z.values[ri:])
             ax.plot(x, z, color='#111111', lw=1, alpha=0.1)
-            ax.scatter(left.x, left.z, s=40, alpha=0.5, color='#ff7f0e', label=legend and 'Left Foot')
-            ax.scatter(right.x, right.z, s=40, alpha=0.5, color='#1f77b4', label=legend and 'Right Foot')
+            ax.scatter(left.x, left.z, s=50, alpha=0.5, color='#ff7f0e', label=legend and 'Left Foot')
+            ax.scatter(right.x, right.z, s=50, alpha=0.5, color='#1f77b4', label=legend and 'Right Foot')
             legend = None
         lmj.plot.legend(loc='upper left')
 
