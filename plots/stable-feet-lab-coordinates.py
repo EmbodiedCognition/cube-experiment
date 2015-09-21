d760d5a09c4d21ae14e3cc7487ba44dbc6934ffd
Leif Johnson <leif@lmjohns3.com>
2015-09-20 22:24:29 -0500

    Get nicer feet plots with lines.

--- plots/stable-feet-lab-coordinates.py
+++ plots/stable-feet-lab-coordinates.py
@@ -9,7 +9,7 @@ import utils
 def load(root, pattern):
     for trial in lmj.cubes.Experiment(root).trials_matching(pattern):
         trial.load()
-        trial.add_velocities(7)
+        trial.add_velocities(19)
         l = trial.trajectory(47, velocity=True)
         l['speed'] = np.sqrt((l[['vx', 'vy', 'vz']] ** 2).sum(axis=1))
         lm = utils.local_minima(l.speed.values)
@@ -25,25 +25,28 @@ def main(root, pattern='*'):
         ax.scatter([0], [0], color='#111111', alpha=0.7, s=200, marker='s', lw=0)
         for lm, left, rm, right in load(root, pattern):
             x, z = [], []
+            li = ri = 0
             while lm and rm:
                 if lm[0] < rm[0]:
-                    x.append(left.iloc[lm[0], 0])
-                    z.append(left.iloc[lm[0], 2])
+                    x.append(left.x.values[li])
+                    z.append(left.z.values[li])
+                    li += 1
                     lm.pop(0)
                 else:
-                    x.append(right.iloc[rm[0], 0])
-                    z.append(right.iloc[rm[0], 2])
+                    x.append(right.x.values[ri])
+                    z.append(right.z.values[ri])
+                    ri += 1
                     rm.pop(0)
             if lm:
-                x.extend(left.iloc[lm, 0])
-                z.extend(left.iloc[lm, 2])
+                x.extend(left.x.values[li:])
+                z.extend(left.z.values[li:])
             if rm:
-                x.extend(right.iloc[rm, 0])
-                z.extend(right.iloc[rm, 2])
-            ax.plot(x, z, color='#111111', lw=1, alpha=0.5)
-            ax.scatter(left.x, left.z, color='#ff7f0e', label=legend and 'Left Foot')
-            ax.scatter(right.x, right.z, color='#1f77b4', label=legend and 'Right Foot')
-            legend = False
+                x.extend(right.x.values[ri:])
+                z.extend(right.z.values[ri:])
+            ax.plot(x, z, color='#111111', lw=1, alpha=0.1)
+            ax.scatter(left.x, left.z, s=40, alpha=0.5, color='#ff7f0e', label=legend and 'Left Foot')
+            ax.scatter(right.x, right.z, s=40, alpha=0.5, color='#1f77b4', label=legend and 'Right Foot')
+            legend = None
         lmj.plot.legend(loc='upper left')
 
 
