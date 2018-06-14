40a881efb638caeb2dc5d3cb95f3b73a2299c0c5
Leif Johnson <leif@lmjohns3.com>
2018-06-13 23:00:52 -0700

    Remove namespace package.

--- setup.py
+++ setup.py
@@ -2,15 +2,12 @@ import os
 import setuptools
 
 setuptools.setup(
-    name='lmj.cubes',
+    name='cubes',
     version='0.0.1',
-    namespace_packages=['lmj'],
     packages=setuptools.find_packages(),
-    author='Leif Johnson',
-    author_email='leif@leifjohnson.net',
     description='analysis code for cube experiment',
     long_description=open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.rst')).read(),
     license='MIT',
-    url='http://github.com/lmjohns3/cube-experiment',
-    install_requires=['climate', 'numpy', 'pandas', 'scipy'],
+    url='http://github.com/EmbodiedCognition/cube-experiment',
+    install_requires=['click', 'numpy', 'matplotlib', 'pandas', 'scipy', 'seaborn'],
 )
--- vizard/suit.py
+++ vizard/suit.py
@@ -1,6 +1,11 @@
 '''Constants related to phasespace markers.'''
 
-# labels for markers reported by the phasespace setup.
+# labels for markers reported by the phasespace setup. abbreviations:
+# mc = metacarpal
+# mt = metatarsal
+# tN = thoracic vertebra #N
+# psis = posterior superior iliac spine
+# asis = anterior superior iliac spine
 MARKER_LABELS = [
     'r-head-back',   # 00
     'r-head-front',
@@ -16,10 +21,10 @@ MARKER_LABELS = [
     'r-fing-ring',
     'r-fing-middle',
     'r-fing-index',
-    'r-mc-outer',
-    'r-mc-inner',    # 15
-    'r-thumb-base',
-    'r-thumb-tip',
+    'r-mc5',
+    'r-mc2',         # 15
+    'r-mc1',
+    'r-thumb',
     'l-collar',
     'l-shoulder',
     'l-elbow',       # 20
@@ -28,30 +33,30 @@ MARKER_LABELS = [
     'l-fing-ring',
     'l-fing-middle',
     'l-fing-index',  # 25
-    'l-mc-outer',
-    'l-mc-inner',
-    'l-thumb-base',
-    'l-thumb-tip',
+    'l-mc5',
+    'l-mc3',
+    'l-mc1',
+    'l-thumb',
     'abdomen',       # 30
     'sternum',
     't3',
     't9',
-    'l-ilium',
-    'r-ilium',       # 35
-    'r-hip',
+    'l-psis',
+    'r-psis',        # 35
+    'r-asis',
     'r-knee',
     'r-shin',
     'r-ankle',
     'r-heel',        # 40
-    'r-mt-outer',
-    'r-mt-inner',
-    'l-hip',
+    'r-mt5',
+    'r-mt1',
+    'l-asis',
     'l-knee',
     'l-shin',        # 45
     'l-ankle',
     'l-heel',
-    'l-mt-outer',
-    'l-mt-inner',
+    'l-mt5',
+    'l-mt1',
     ]
 
 
